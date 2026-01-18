97fe4b15713db2f40f0ffba2543fc952f3de19eb
DearYoyoa <99875576+DearYoyoa@users.noreply.github.com>
2026-01-18 22:19:02 +0800

    Update classifier_test.py

--- classifier_train/classifier_test.py
+++ classifier_train/classifier_test.py
@@ -5,41 +5,32 @@ import numpy as np
 from tqdm import tqdm
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score, precision_score, recall_score, f1_score
-import joblib  # 用于加载模型
+import joblib 
 import torch
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 def load_classifier(model_path):
-    """
-    加载逻辑回归分类器模型。
-    """
+
     classifier = joblib.load(model_path)
     return classifier
 
 def query_with_image(classifier, image_hidden_state, original_hidden_state, last_hidden):
-    """
-    使用逻辑回归分类器进行预测。
-    """
-    # 只使用第一个图像的表征
+
     last_hidden = last_hidden[-1]
     original_hidden_state = original_hidden_state[0]
     # print("last_hidden.shape", last_hidden.shape)
     # print("original_hidden_state.shape", original_hidden_state.shape)
     x = torch.cat((torch.tensor(last_hidden), torch.tensor(original_hidden_state)), dim=0).to(DEVICE)
-    x = x.reshape(1, -1).cpu().numpy()  # 将特征展平为二维数组
+    x = x.reshape(1, -1).cpu().numpy() 
     prediction = classifier.predict(x)[0]
-    prediction_score = classifier.predict_proba(x)[0][1]  # 获取正类的概率分数
+    prediction_score = classifier.predict_proba(x)[0][1]
     return prediction, prediction_score
 
 def main(args):
-    # 加载逻辑回归模型
+
     classifier = load_classifier(args.classifier_path)
-
-    # 创建输出目录
     os.makedirs(f'{args.output_root}/', exist_ok=True)
-
-    # 加载测试数据
     with open('data_okvqa/okvqa_input.json', 'r') as f:
         data = json.load(f)
 
@@ -61,10 +52,8 @@ def main(args):
         image_hidden = np.load(f"hidden_state_i2_test/hidden_okvqa/hidden_state/{sample['image_id']}.npy")
         original_hidden = np.load(f"hidden_state_i2_test/hidden_okvqa/original_hidden_state/{sample['image_id']}.npy")
 
-        # 使用逻辑回归分类器进行预测
         prediction, prediction_score = query_with_image(classifier, image_hidden, original_hidden, last_hidden)
 
-        # 获取真实标签
         answer_in_pred = None
         with open('data_okvqa/okvqa/okvqa_i2_test/Qwen25_72B_Instruct_awq_vllm_l20_judged_logs_okvqa_i2_test.json', 'r') as f:
             test_data = json.load(f)
@@ -94,9 +83,7 @@ def main(args):
         }
         logs.append(log_entry)
 
-    # 计算AUC
     auc_score = roc_auc_score(true_labels, prediction_scores)
-    # 计算Precision, Recall, F1
     precision = precision_score(true_labels, predictions)
     recall = recall_score(true_labels, predictions)
     f1 = f1_score(true_labels, predictions)
