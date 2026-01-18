d843c25be9da179467b41454514ec5ea9e93cce1
DearYoyoa <99875576+DearYoyoa@users.noreply.github.com>
2026-01-18 22:10:49 +0800

    Update cl2_token_evqa.sh

--- cl2_token_evqa.sh
+++ cl2_token_evqa.sh
@@ -3,7 +3,6 @@
 # DATA_DIR="adaptive"
 # OUTPUT_DIR="local_data/infoseek_qwen"
 
-# # 运行自适应检索实验
 # echo "Running Adaptive Retrieval Experiment..."
 
 # # Define the layers, epochs, and model types
@@ -56,26 +55,22 @@
 
 # !/bin/bash
 
-# 定义基础文件夹路径
-BASE_FOLDER="data_1017/evqa_i2_ablation_text"  # 要遍历的文件夹路径
-OUTPUT_ROOT="data_1017/evqa_i2_ablation_text"  # 输出根路径
+BASE_FOLDER="data_1017/evqa_i2_ablation_text"
+OUTPUT_ROOT="data_1017/evqa_i2_ablation_text"
 
-# 遍历 BASE_FOLDER 下的每个文件夹
 for folder in "$BASE_FOLDER"/*; do
   if [ -d "$folder" ]; then
-    # 遍历文件夹中的每个 .json 文件
+
     for json_file in "$folder"/*.json; do # 8 16的9~16 original_image_wo_rir
       if [[ "$(basename "$json_file")" == *"evqa_i2_probe_token_layer_-1_"* ]]; then
         if [ -f "$json_file" ]; then
-          # 获取文件名去除扩展名，作为 EXP_NAME
+
           EXP_NAME=$(basename "$json_file" .json)
           echo "Processing EXP_NAME: $EXP_NAME"
-          
-          # 生成命令，不添加 logs_ 前缀
+
           COMMAND="python -m experiment.evqa.judge_qwen --output_root $folder --exp_name $EXP_NAME"
           echo "Generated command: $COMMAND"
-          
-          # 执行命令
+
           $COMMAND
         fi
       fi
