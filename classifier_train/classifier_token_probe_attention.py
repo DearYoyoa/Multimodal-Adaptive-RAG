1dccda9b568581ee883db8033645dac492f97ec7
DearYoyoa <99875576+DearYoyoa@users.noreply.github.com>
2026-01-18 22:23:28 +0800

    Update classifier_token_probe_attention.py

--- classifier_train/classifier_token_probe_attention.py
+++ classifier_train/classifier_token_probe_attention.py
@@ -13,11 +13,7 @@ import torch.nn.functional as F
 class TokenProbeDataset(Dataset):
     def __init__(self, json_file, image_hidden_state_dir, original_image_hidden_state_dir, 
                  hidden_state_dir=None, hidden_state_rir_dir=None, layer_idx=-1):
-        """
-        Args:
-            layer_idx (int): 指定使用第几层的hidden state。默认为-1，表示最后一层。
-                           范围应该在0到32之间（总共33层，索引从0开始）
-        """
+
         with open(json_file, 'r') as f:
             self.data = json.load(f)
         self.image_hidden_state_dir = image_hidden_state_dir
@@ -33,13 +29,13 @@ class TokenProbeDataset(Dataset):
         item = self.data[idx]
         image_id = item['image_id']
         
-        # 加载image hidden states - (33, 2, 4096)
+        # image hidden states - (33, 2, 4096)
         image_hidden_state = np.load(os.path.join(self.image_hidden_state_dir, f"{image_id}.npy"))
         # print("imgae_hidden_state:",image_hidden_state.shape)
-        # 加载original image hidden states - (2, 4096)
+        # original image hidden states - (2, 4096)
         original_image_hidden_state = np.load(os.path.join(self.original_image_hidden_state_dir, f"{image_id}.npy"))
         # print("original_image_hidden_state:",original_image_hidden_state.shape)
-        # 加载text hidden states
+        # text hidden states
         hidden_state = None
         if self.hidden_state_dir:
             hidden_state = np.load(os.path.join(self.hidden_state_dir, f"{image_id}.npy"))
@@ -48,8 +44,7 @@ class TokenProbeDataset(Dataset):
         hidden_state_rir = None
         if self.hidden_state_rir_dir:
             hidden_state_rir = np.load(os.path.join(self.hidden_state_rir_dir, f"hidden_states_rir_{image_id}.npy"))
-        
-        # 获取标签
+
         # label = 1 if item['group'].endswith('_0') else 0
         label = 0 if item['group'].endswith('_0') else 1
         group_label = int(item['group'].split('_')[0]) * 2 + (1 if item['group'].endswith('_1') else 0)
@@ -94,46 +89,40 @@ def train(model, dataloader, criterion, optimizer, device, epoch, model_type, nu
         # x = image_hidden[layer_idx, 0] 
         # print("x.shape:", x.shape)
 
-        # 根据模型类型选择输入
         if 'original' in model_type:
             if 'wo_rir' in model_type:
-                # 只使用第一个图像的表征
+
                 x = original_image_hidden[:, 0]  # (batch_size, 4096)
             else:
-                # 使用所有表征
+
                 x = original_image_hidden.reshape(original_image_hidden.size(0), -1)  # (batch_size, 8192)
         else:
             if 'wo_rir' in model_type:
-                # 只使用指定层第一个图像的表征
+
                 # x = image_hidden[:, layer_idx, 0]  # (batch_size, 4096)
                 x = image_hidden[:, layer_idx, 0]
                 # print("x:",x.shape)
             else:
-                # 使用指定层所有表征
+
                 x = image_hidden[:, layer_idx].reshape(image_hidden.size(0), -1)  # (batch_size, 8192)
 
-            
-        # 选择文本表征
         if 'wo_rir' in model_type:
             text_hidden = hidden_state
         else:
-            # 使用两种文本表征
+
             text_hidden = torch.cat([
                 hidden_state.squeeze(1).squeeze(1),  # (batch_size, 4096)
                 hidden_state_rir.squeeze(1).squeeze(1)  # (batch_size, 4096)
             ], dim=1)  # (batch_size, 8192)
         # x = text_hidden
 
-        # 计算交叉注意力
         text_hidden = text_hidden.unsqueeze(0).to(device)  # (1, batch_size, input_dim)
         # text_hidden = text_hidden.permute(1, 0, 2)
         # print("text_hidden:",text_hidden.shape)
         x = x.unsqueeze(0)  # (1, batch_size, input_dim)
         x = x.squeeze(0).permute(1, 0, 2)  # → shape: (324, 32, 4096)
-
         # print("x:", x.shape)
 
-        # 使用文本特征作为查询，视觉特征作为键和值
         attn_output, attn_weights = nn.MultiheadAttention(embed_dim=x.size(-1), num_heads=1)(text_hidden, x, x)  # (1, batch_size, input_dim)
         attn_output = attn_output.squeeze(0)  # (batch_size, input_dim)
         # print("attn_output:", attn_output.shape)
@@ -142,7 +131,6 @@ def train(model, dataloader, criterion, optimizer, device, epoch, model_type, nu
         batch_indices = torch.arange(x_transposed.size(0), device=x.device)
         max_patch_tokens = x_transposed[batch_indices, max_indices]
 
-        # 合并注意力输出和文本特征
         combined_output = torch.cat((max_patch_tokens, text_hidden.squeeze(0)), dim=1)  # (batch_size, input_dim * 2)
 
 
@@ -175,10 +163,9 @@ def save_model(model, model_name, epoch, token_type):
     logging.info(f"Saved model to {save_path}")
 
 def main():
-    # 设置日志
+
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-    # 设置参数
     batch_size = 16
     num_epochs = 20
 
@@ -189,8 +176,8 @@ def main():
     # exact_answer_last_token -1 RAG3 okvqa_train_attention.log 
     # exact_answer_last_token 30 RAG3 okvqa_train_attention.log learning_rate 0.0005
     # exact_answer_first_token 30 RAG3 okvqa_train_attention.log learning_rate 0.001
-    # 为每一层创建数据集和训练模型
-    # layer_indices = [30]  #      示例：选择几个关键层进行实验
+
+    # layer_indices = [30]  
     layer_indices = [-1]
     exact_tokens = [
         'exact_answer_last_token',
@@ -207,8 +194,7 @@ def main():
     # exact_tokens = [
     #     'exact_answer_before_first_token'
     # ]
-    
-    # 训练两种分类任务
+
     for num_classes in [2]:
         logging.info(f"\nTraining {num_classes}-class classification models")
         for token_type in exact_tokens:
@@ -216,8 +202,7 @@ def main():
         
             for layer_idx in layer_indices:
                 logging.info(f"\nTraining models for layer {layer_idx}")
-                
-                # 创建数据集
+
                 dataset = TokenProbeDataset(
                     # json_file='data_0921/infoseek/infoseek_i2_rir/logs_infoseek_i2_rir.json',
                     json_file='okvqa_train_data/okvqa/okvqa_train_i2_rir/logs_okvqa_train_i2_rir.json',
@@ -229,26 +214,23 @@ def main():
                 )
                 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-                # 定义模型配置
                 task_prefix = 'binary' if num_classes == 2 else 'multiclass'
                 model_configs = [
-                    # 不使用RIR的模型 (输入维度 4096 + 4096 = 8192)
+                    #  (4096 + 4096 = 8192)
 
                     # (f'qwen_dp3_{task_prefix}_layer_{layer_idx}_original_image_wo_rir_{token_type}', 4096, num_classes),
                     (f'qwen_dp3_{task_prefix}_layer_{layer_idx}_image_wo_rir_{token_type}', 8192, num_classes),
                     # (f'qwen_dp3_{task_prefix}_layer_{layer_idx}_image_wo_rir_{token_type}', 4096, num_classes),
 
-                    # 使用所有表征的模型 (输入维度 8192 + 8192 = 16384)
+                    #  ( 8192 + 8192 = 16384)
                     # (f'qwen_dp3_{task_prefix}_layer_{layer_idx}_original_image_rir', 16384, num_classes),
                     # (f'qwen_dp3_{task_prefix}_layer_{layer_idx}_image_rir', 16384, num_classes)
                 ]
 
-                # 训练每个模型
                 for model_name, input_dim, num_classes in model_configs:
                     logging.info(f"\nTraining {model_name}...")
                     model = MLPClassifier(input_dim, num_classes).to(device)
-                    
-                    # 设置损失函数和优化器
+
                     if num_classes == 2:
                         criterion = nn.CrossEntropyLoss()
                     else:
@@ -268,8 +250,8 @@ def main():
 if __name__ == "__main__":
     main() 
 
-# i3每组的样本数量：
-# 组 (False, False): 348 个样本
-# 组 (False, True): 129 个样本
-# 组 (True, False): 167 个样本
-# 组 (True, True): 356 个样本
+# i3：
+#  (False, False): 348 sample
+#  (False, True): 129 sample
+#  (True, False): 167 sample
+#  (True, True): 356 sample
